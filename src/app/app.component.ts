f1f95c7722ef4ad6e9459f662b98094695c344bf
Jean-Yves Rosaye <jrosaye@hotmail.com>
2025-03-12 20:57:50 +0100

    Layout component generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'webJO24';
+  title = 'JO-FrontEnd';
 }
