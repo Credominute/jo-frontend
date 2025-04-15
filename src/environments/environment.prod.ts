9f56cd6b9f0c49bfbf779bfc444ecaec3faa01a6
Jean-Yves Rosaye <jrosaye@hotmail.com>
2025-04-15 13:55:11 +0200

    Config of environment.prod.ts for Netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://afternoon-lowlands-41903-d5dc47129290.herokuapp.com/api'
+    api: 'https://afternoon-lowlands-41903-d5dc47129290.herokuapp.com/api'
   };
