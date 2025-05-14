c57ac2cdf09b455716600141cd2f6ca80d742ac8
Jean-Yves Rosaye <jrosaye@hotmail.com>
2025-05-14 23:01:39 +0200

    Update ticket model with QR code and adjust rendering

--- src/app/component/order/order.component.ts
+++ src/app/component/order/order.component.ts
@@ -1,19 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Order } from '../../models/order.model';
-import { TicketComponent } from '../ticket/ticket.component';
+import { DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-order',
   standalone: true,
   templateUrl: './order.component.html',
-  styleUrls: ['../../../scss/components/order.scss'], // ✅ corrigé (styleUrls au pluriel)
-  imports: [CommonModule, TicketComponent]
+  styleUrls: ['../../../scss/components/order.scss'],
+  imports: [CommonModule]
 })
 export class OrderComponent {
-  @Input() order!: Order; // j'évite `new Order()` (les interfaces ne sont pas des classes)
+  @Input() order!: Order; 
   showDetailsOrder = false;
   showTicketOrder = false;
+
+  constructor(private readonly sanitizer: DomSanitizer) {}
+
+  ngOnChanges(): void {
+    if (this.order?.tickets) {
+      this.order.tickets.forEach(ticket => {
+        ticket.safeQrcodeUrl = this.sanitizer.bypassSecurityTrustUrl(
+          'data:image/png;base64,' + ticket.qrcode
+        );
+      });
+    }
+  }
 
   showDetails(): void {
     this.showDetailsOrder = !this.showDetailsOrder;
