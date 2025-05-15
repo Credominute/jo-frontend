5eab8bf0045a77034c569b576ec206dc6bdd8668
Jean-Yves Rosaye <jrosaye@hotmail.com>
2025-05-15 17:54:57 +0200

    Fix: mockPayment added to production & update index.scss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     api: 'https://afternoon-lowlands-41903-d5dc47129290.herokuapp.com/',
-    mock: false
+    mock: false,
+    mockPayment: true
   };
